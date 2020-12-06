95176839805db43a91a10968e428761cca472288
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2020-12-06 16:20:16 +0100

    cleanup

--- d5.rb
+++ d5.rb
@@ -17,15 +17,16 @@ def decode(seat)
   [row, col]
 end
 
+def seat_num(row, col) row * 8 + col end
+
 input = File.readlines("d5.txt").map { |x| x.strip() }
-seat_numbers = input.map { |x| decode(x) }.map { |(row, col)| row * 8 + col }
-puts "Part 1 %s" % [seat_numbers.sort[-1]]
+seat_nums = input.map { |x| decode(x) }.map { |x| seat_num(*x) }
+puts "Part 1 %s" % [seat_nums.sort[-1]]
 
 all_seats = (0..127).to_a.product((0..7).to_a)
 taken_seats = input.map { |x| decode(x) }
 candidate_seats = all_seats.difference(taken_seats)
-row, col = candidate_seats.find { |row, col|
-  seat_num = row * 8 + col
-  seat_numbers.include?(seat_num - 1) && seat_numbers.include?(seat_num + 1)
+row, col = candidate_seats.find { |s|
+  seat_nums.include?(seat_num(*s) - 1) && seat_nums.include?(seat_num(*s) + 1)
 }
 puts "Part 2 %d" % [row * 8 + col]
