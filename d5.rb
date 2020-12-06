0984d89bbb8aed829fae487881f418aef67ebdc3
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2020-12-06 16:14:04 +0100

    d5 pt 2

--- d5.rb
+++ d5.rb
@@ -14,8 +14,18 @@ def decode(seat)
       { from: ((acc[:from] + acc[:to]) / 2.0).ceil, to: acc[:to] }
     end
   }[:from]
-  row * 8 + col
+  [row, col]
 end
 
 input = File.readlines("d5.txt").map { |x| x.strip() }
-puts "Part 1 %s" % [input.map { |x| decode(x) }.sort[-1]]
+seat_numbers = input.map { |x| decode(x) }.map { |(row, col)| row * 8 + col }
+puts "Part 1 %s" % [seat_numbers.sort[-1]]
+
+all_seats = (0..127).to_a.product((0..7).to_a)
+taken_seats = input.map { |x| decode(x) }
+candidate_seats = all_seats.difference(taken_seats)
+row, col = candidate_seats.find { |row, col|
+  seat_num = row * 8 + col
+  seat_numbers.include?(seat_num - 1) && seat_numbers.include?(seat_num + 1)
+}
+puts "Part 2 %d" % [row * 8 + col]
