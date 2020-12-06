7b220fa8bae242b5fe27f87531a414f00992d900
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2020-12-06 22:49:36 +0100

    d6 pt2

--- d6.rb
+++ d6.rb
@@ -1,9 +1,9 @@
 require "set"
 
-r = File
+groups = File
   .read("d6.txt")
   .split("\n\n")
-  .map { |x| x.split("\n").map { |y| y.split("") }.flatten }
-  .map { |x| Set.new(x).size }
-  .inject(:+)
-p r
+  .map { |x| x.split("\n").map { |y| y.split("") } }
+
+puts "Part 1: %d" % [groups.map { |x| Set.new(x.flatten).size }.inject(:+)]
+puts "Part 2: %d" % [groups.map { |g| g.inject(:&).length }.inject(:+)]
