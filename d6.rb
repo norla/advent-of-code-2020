8d475315b195256c20b955b26c05f7f2142eb90b
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2020-12-07 07:39:09 +0100

    cleanup

--- d6.rb
+++ d6.rb
@@ -1,9 +1,7 @@
-require "set"
-
 groups = File
   .read("d6.txt")
   .split("\n\n")
   .map { |x| x.split("\n").map { |y| y.split("") } }
 
-puts "Part 1: %d" % [groups.map { |x| Set.new(x.flatten).size }.inject(:+)]
+puts "Part 1: %d" % [groups.map { |g| g.flatten.uniq.size }.inject(:+)]
 puts "Part 2: %d" % [groups.map { |g| g.inject(:&).length }.inject(:+)]
