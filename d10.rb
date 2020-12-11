5adac263aeb6fc1f5b12b67eb1755e0c42fa7134
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2020-12-11 10:05:21 +0100

    d10 part 1

--- d10.rb
+++ d10.rb
@@ -1,12 +1,12 @@
-input = File.readlines("d10.txt").map{ |l| l.to_i }
-adapters  = input + [input.max + 3]
+input = File.readlines("d10.txt").map { |l| l.to_i }
+adapters = input + [input.max + 3]
 
 def run(joltage, used, unused)
   if unused.size == 0
     return used
   end
-  suitable = unused.filter{ |u|  joltage.between?(u-3, u-1) }
-  suitable.each{ |s|
+  suitable = unused.filter { |u| joltage.between?(u - 3, u - 1) }.sort
+  suitable.each { |s|
     diff = s - joltage
     res = run(s, used + [[s, diff]], unused - [s])
     if res
@@ -15,9 +15,8 @@ def run(joltage, used, unused)
   }
   p unused
   return nil
-
 end
 
 # require 'pp'
 res = run(0, [], adapters)
-puts "Part 1", res.filter{ | diff| diff[1] == 3 }.length , res.filter{ | diff| diff[1] == 1 }.length
+puts "Part 1", res.filter { |diff| diff[1] == 3 }.length, res.filter { |diff| diff[1] == 1 }.length
